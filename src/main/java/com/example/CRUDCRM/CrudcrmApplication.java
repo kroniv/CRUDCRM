367e1cef9a5b84c254fe7614d6925c7ea9b441cd
Alexander Blinov <kroniv@gmail.com>
2023-03-07 13:48:08 +0500

    init

---
+++ src/main/java/com/example/CRUDCRM/CrudcrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDCRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudcrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudcrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CRUDCRM/CrudcrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CRUDCRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudcrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
